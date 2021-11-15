e16aed8fbc732bdef3970ff50d563b38ba26648e
geninhocell <geninhocell@gmail.com>
2021-11-15 19:55:59 -0300

    buscando caminho da foto do banco

--- app/src/main/java/com/alura/agenda/FormularioHelper.java
+++ app/src/main/java/com/alura/agenda/FormularioHelper.java
@@ -1,6 +1,9 @@
 package com.alura.agenda;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.RatingBar;
 
 import com.alura.agenda.modelo.Aluno;
@@ -11,6 +14,7 @@ public class FormularioHelper {
     private final EditText campoTelefone;
     private final EditText campoSite;
     private final RatingBar campoNota;
+    private final ImageView campoFoto;
 
     private Aluno aluno;
 
@@ -20,6 +24,7 @@ public class FormularioHelper {
         campoTelefone = activity.findViewById(R.id.formulario_telefone);
         campoSite = activity.findViewById(R.id.formulario_site);
         campoNota = activity.findViewById(R.id.formulario_nota);
+        campoFoto = activity.findViewById(R.id.formulario_foto);
 
         aluno = new Aluno();
     }
@@ -30,6 +35,7 @@ public class FormularioHelper {
         aluno.setTelefone(campoTelefone.getText().toString());
         aluno.setSite(campoSite.getText().toString());
         aluno.setNota((double) campoNota.getProgress());
+        aluno.setCaminhoFoto((String) campoFoto.getTag());
 
         return aluno;
     }
@@ -40,6 +46,17 @@ public class FormularioHelper {
         campoTelefone.setText(aluno.getTelefone());
         campoSite.setText(aluno.getSite());
         campoNota.setProgress(aluno.getNota().intValue());
+        carregaImagem(aluno.getCaminhoFoto());
         this.aluno = aluno;
+    }
+
+    public void carregaImagem(String caminhoFoto) {
+        if(caminhoFoto != null) {
+            Bitmap bitmap = BitmapFactory.decodeFile(caminhoFoto);
+            Bitmap bitmapReduzido = Bitmap.createScaledBitmap(bitmap, 300, 300, true);
+            campoFoto.setImageBitmap(bitmapReduzido);
+            campoFoto.setScaleType(ImageView.ScaleType.FIT_XY);
+            campoFoto.setTag(caminhoFoto);
+        }
     }
 }
--- app/src/main/java/com/alura/agenda/dao/AlunoDAO.java
+++ app/src/main/java/com/alura/agenda/dao/AlunoDAO.java
@@ -18,20 +18,32 @@ import java.util.List;
 public class AlunoDAO extends SQLiteOpenHelper {
 
     public AlunoDAO(@Nullable Context context) {
-        super(context, "Agenda", null, 1);
+        super(context, "Agenda", null, 2);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        String sql = "CREATE TABLE Alunos (id INTEGER PRIMARY KEY, nome TEXT NOT NULL, endereco TEXT, telefone TEXT, site TEXT, nota REAL);";
+        String sql = "CREATE TABLE Alunos (id INTEGER PRIMARY KEY, " +
+                "nome TEXT NOT NULL, " +
+                "endereco TEXT, " +
+                "telefone TEXT, " + "" +
+                "site TEXT, " +
+                "nota REAL, " +
+                "caminhoFoto TEXT);";
         db.execSQL(sql);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        String sql = "DROP TABLE IF EXISTS Alunos;";
-        db.execSQL(sql);
-        onCreate(db);
+//        String sql = "DROP TABLE IF EXISTS Alunos;";
+        String sql = "";
+        switch (oldVersion){
+            case 1:
+                sql = "ALTER TABLE Alunos ADD COLUMN caminhoFoto TEXT;";
+                db.execSQL(sql);
+        }
+
+//        onCreate(db);
     }
 
     public void insere(Aluno aluno) {
@@ -50,6 +62,7 @@ public class AlunoDAO extends SQLiteOpenHelper {
         dados.put("telefone", aluno.getTelefone());
         dados.put("site", aluno.getSite());
         dados.put("nota", aluno.getNota());
+        dados.put("caminhoFoto", aluno.getCaminhoFoto());
         return dados;
     }
 
@@ -68,6 +81,7 @@ public class AlunoDAO extends SQLiteOpenHelper {
             aluno.setTelefone(cursor.getString(cursor.getColumnIndex("telefone")));
             aluno.setSite(cursor.getString(cursor.getColumnIndex("site")));
             aluno.setNota(cursor.getDouble(cursor.getColumnIndex("nota")));
+            aluno.setCaminhoFoto(cursor.getString(cursor.getColumnIndex("caminhoFoto")));
 
             alunos.add(aluno);
         }
