4e479526f63258d84d3ddfb943814ecf0fb30d67
geninhocell <geninhocell@gmail.com>
2021-11-15 13:00:41 -0300

    sms, mapa

--- app/src/main/java/com/alura/agenda/MainActivity.java
+++ app/src/main/java/com/alura/agenda/MainActivity.java
@@ -1,14 +1,11 @@
 package com.alura.agenda;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-
 import com.alura.agenda.dao.AlunoDAO;
 import com.alura.agenda.modelo.Aluno;
-
 import androidx.appcompat.app.AppCompatActivity;
-
-
 import android.view.ContextMenu;
 import android.view.MenuItem;
 import android.view.View;
@@ -16,8 +13,6 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
-import android.widget.Toast;
-
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
@@ -32,14 +27,11 @@ public class MainActivity extends AppCompatActivity {
 
         listaAlunos = findViewById(R.id.lista_alunos);
 
-        listaAlunos.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> lista, View item, int position, long id) {
-                Aluno aluno = (Aluno) listaAlunos.getItemAtPosition(position);
-                Intent intentVaiFormulario = new Intent(MainActivity.this, FormularioActivity.class);
-                intentVaiFormulario.putExtra("aluno", aluno);
-                startActivity(intentVaiFormulario);
-            }
+        listaAlunos.setOnItemClickListener((lista, item, position, id) -> {
+            Aluno aluno = (Aluno) listaAlunos.getItemAtPosition(position);
+            Intent intentVaiFormulario = new Intent(MainActivity.this, FormularioActivity.class);
+            intentVaiFormulario.putExtra("aluno", aluno);
+            startActivity(intentVaiFormulario);
         });
 
         Button novoAluno = findViewById(R.id.novo_aluno);
@@ -67,17 +59,42 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
-       MenuItem deletar = menu.add("Deletar");
-       deletar.setOnMenuItemClickListener(item -> {
-           AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) menuInfo;
-           Aluno aluno = (Aluno) listaAlunos.getItemAtPosition(info.position);
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) menuInfo;
+        final Aluno aluno = (Aluno) listaAlunos.getItemAtPosition(info.position);
+        
+        MenuItem itemSMS = menu.add("Enviar SMS");
+        Intent intentSMS = new Intent(Intent.ACTION_VIEW);
+        intentSMS.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intentSMS.setData(Uri.parse("sms:" + aluno.getTelefone()));
+        itemSMS.setIntent(intentSMS);
 
-           AlunoDAO alunoDAO = new AlunoDAO(this);
-           alunoDAO.deleta(aluno);
-           alunoDAO.close();
+        MenuItem itemMapa = menu.add("Visualizar no mapa");
+        Intent intentMapa = new Intent(Intent.ACTION_VIEW);
+        intentMapa.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intentMapa.setData(Uri.parse("geo:0,0?q=" + aluno.getEndereco()));
+        itemMapa.setIntent(intentMapa);
 
-           carregaLista();
-           return false;
-       });
+        MenuItem itemSite = menu.add("Visitar site");
+        Intent intentSite = new Intent(Intent.ACTION_VIEW);
+        intentSite.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+
+
+        String site = aluno.getSite();
+        if(!site.startsWith("https://")){
+            site = "https://" + site;
+        }
+
+        intentSite.setData(Uri.parse(site));
+        itemSite.setIntent(intentSite);
+
+        MenuItem deletar = menu.add("Deletar");
+        deletar.setOnMenuItemClickListener(item -> {
+            AlunoDAO alunoDAO = new AlunoDAO(this);
+            alunoDAO.deleta(aluno);
+            alunoDAO.close();
+            carregaLista();
+            return false;
+        });
     }
 }
