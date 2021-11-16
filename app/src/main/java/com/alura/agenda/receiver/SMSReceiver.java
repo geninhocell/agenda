77cafa224a930ea438e467a1ec72d82d20a6de4d
geninhocell <geninhocell@gmail.com>
2021-11-16 00:26:28 -0300

    tratando evento de recebimento de sms

---
+++ app/src/main/java/com/alura/agenda/receiver/SMSReceiver.java
@@ -0,0 +1,13 @@
+package com.alura.agenda.receiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class SMSReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Toast.makeText(context, "Chegou um SMS!", Toast.LENGTH_SHORT).show();
+    }
+}
